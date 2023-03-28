32f7179812947f2f03261212ce39aa2712f48760
qun550 <123520147+qun550@users.noreply.github.com>
2023-03-28 16:58:50 -0400

    exercise 07

---
+++ exercises/exercise-07/main.js
@@ -0,0 +1,21 @@
+const generatedSentence = document.getElementById('generated-sentence');
+const generateBtn = document.getElementById('generate-btn');
+
+const nouns = ['heart', 'rainbow', 'ocean', 'flower', 'sun'];
+const adjectives = ['beautiful', 'bold', 'graceful', 'shining', 'excited'];
+const verbs = ['see', 'discover', 'notice', 'admire', 'observe'];
+
+function generateRandomSentence() {
+    const noun = nouns[Math.floor(Math.random() * nouns.length)];
+    const adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+    const verb = verbs[Math.floor(Math.random() * verbs.length)];
+
+    const sentence = `My ${noun} leaps ${adjective} when I ${verb} a rainbow in the sky.`;
+    return sentence;
+}
+
+generateBtn.addEventListener('click', () => {
+    generatedSentence.textContent = generateRandomSentence();
+});
+
+generatedSentence.textContent = generateRandomSentence();
